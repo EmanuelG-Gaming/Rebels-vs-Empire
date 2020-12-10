921cc54f004eb2312ef41b1f7c547451577b452b
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2020-12-10 17:51:34 +0200

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,25 +1,5 @@
- // Weapons
- const eclipseSDSuperLaser = new Weapon();
- 
- // Bullets
- const eclipseSDSuperLaser = extend(LaserBulletType, {});
-} 
-
-eclipseSDSuperLaser.lightningSpacing = 15
-eclipseSDSuperLaser.width = 250
-eclipseSDSuperLaser.length = 400
-eclipseSDSuperLaser.lightningDelay = 15
-eclipseSDSuperLaser.damage = Number.MAX_VALUE - 1;
-eclipseSDSuperLaser.lightningColor = Pal.remove;
-eclipseSDSuperLaser.colors = [ Pal.remove, Color.green];
-
-//eclipseSD
-eclipseSD.speed = 1
-eclipseSD.health = 5000000;
-eclipseSD.hitSize = 100;
-
-//Units
-require("unit/imperial");
+// Units
+require ("units/imperial");
 
 
 
